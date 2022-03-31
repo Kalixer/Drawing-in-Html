e8ba1e936e26440143b3127eba5372ecdff95e79
Kalixer <al.caceres.ag72@gmail.com>
2022-03-31 20:12:29 -0300

    Lines are drew one by one

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 import draw from "./draw.js"
 
 
@@ -7,8 +8,12 @@ draw(499, 499, 1, 499, "black");
 draw(499, 1, 499, 499, "black");
 
 const boton = document.getElementById("botoncito")
-const d = boton.addEventListener('click', drawing)
+const d = boton.addEventListener('click', drawLines)
 
+let execution;
+let intervalo;
+let counter = 0;
+let a = 1;
 
 function drawing() {
     let x1 = document.getElementById("Color1")
@@ -17,24 +22,33 @@ function drawing() {
     let hex2 = x2.value;
     let x3 = document.getElementById("Color3")
     let hex3 = x3.value;
+    
+    counter++;
+    
+    if(counter == 25){
+        clearInterval(intervalo)
+    }
 
-    for(let i=0; i<25; i++) {
-        let a = i;
-        a = a * 20;
-        draw(0, 500-a, a, 0, hex3);
-        draw(a, 500, 500, 500-a, hex3);
-    }
-    for(let i=0; i<25; i++) {
-        let a = i;
-        a = a * 20;
-        draw(0, a, a, 500, hex1);
-    }
-    for(let i=0; i<25; i++) {
-        let a = i;
-        a = a * 20;
-        draw(a, 0, 500, a, hex2);
-    }
+    a = counter * 20;
+    
+    draw(500 - a, 500, 0, 500 - a, hex1); // 1st
+    draw(a, 0, 500, a, hex2); // 2nd
+    
+    draw(0, 500 - a, a, 0, hex3) // 3rd
+    draw(500, 0 + a, 500 - a, 500, hex3) //4th
+    
 }
+
+function drawLines() {
+    
+    if(!execution) {
+        intervalo = setInterval(drawing, 100)
+    }
+
+}
+
+
+
 
 
 
