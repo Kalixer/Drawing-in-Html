264ae37df749ba3145bea39228764b29e3ee8085
Kalixer <al.caceres.ag72@gmail.com>
2023-03-04 17:35:14 -0300

    Added a feature that reloads the draw with the same button. The message generated below the canvas was deleted

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 "use strict";
 import draw from "./draw.js"
 
-
 draw(1, 1, 1, 499, "black");
 draw(1, 1, 499, 1, "black");
 draw(499, 499, 1, 499, "black");
 draw(499, 1, 499, 499, "black");
 
-const boton = document.getElementById("botoncito")
-const d = boton.addEventListener('click', drawLines)
+let lienzo = document.getElementById('papel')
+let context = lienzo.getContext('2d')
 
-let execution;
+const boton_dibujar = document.getElementById("boton_dibujar")
+const d = boton_dibujar.addEventListener('click', drawLines)
+
+let execution = 0;
 let intervalo;
+
 let counter = 0;
 let a = 1;
 
+
+// This the function that draws, but is called by another functions
 function drawing() {
+    if(counter == 25){
+        // Here the fuction breaks with 'clearInterval'
+        clearInterval(intervalo)
+
+        setTimeout(() => {
+            boton_dibujar.innerText = 'Reset';
+        }, 500)
+    }
+
+    // This is to get the colors
     let x1 = document.getElementById("Color1")
     let hex1 = x1.value;
     let x2 = document.getElementById("Color2")
@@ -24,16 +39,6 @@ function drawing() {
     let hex3 = x3.value;
     
     counter++;
-    
-    if(counter == 25){
-        clearInterval(intervalo)
-
-        setTimeout(() => {
-            const phrase = document.getElementById("lastMessage")
-            phrase.innerHTML = "Pretty, isn't it?";
-        }, 2000)
-    }
-
     a = counter * 20;
     
     draw(500 - a, 500, 0, 500 - a, hex1); // 1st
@@ -45,9 +50,23 @@ function drawing() {
 }
 
 function drawLines() {
+    /* 
+    setInterval will run until 'clearInterval' is called. 
+
+    The 1st parameter calls the function and the 2nd is the time wait in miliseconds until it's called again
     
-    if(!execution) {
-        intervalo = setInterval(drawing, 100)
+    It is declared a variable because it has to be cleared to stop
+    */
+    if(execution == 0) {
+        intervalo = setInterval(drawing, 50)
+        execution = 1
+    } else {
+        context.clearRect(0, 0, 500, 500) // Reset the canvas
+
+        execution = 0
+        counter = 0
+        
+        boton_dibujar.innerText = 'Draw';
     }
 
 }
