6c9a0a1db9d035e41394df22c572004a35d621cb
Kalixer <al.caceres.ag72@gmail.com>
2023-04-09 19:52:55 -0400

    [Milestone] React App working. Now you have to make the necessary components

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <h1>Ola k ase</h1>
+    );
+}
+
+export default Home;
---
+++ src/routes/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Home from "../components/Home";
+
+import "../../styles/global.css"
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route exact path="/" element={ <Home/> }/>
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    )
+}
+
+export default App;
